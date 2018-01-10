2ead49e10dfdc308f183635b60692d3a2ed1a1f2
lucashespanhol <lukas_bora@yahoo.com.br>
2018-01-09 22:26:05 -0200

    Update MainActivity.java
    Testando conflitos

--- app/src/main/java/lucashespanhol/com/br/testegithub/MainActivity.java
+++ app/src/main/java/lucashespanhol/com/br/testegithub/MainActivity.java
@@ -4,10 +4,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    
+    private int a;
+    private int b;
+    private int c;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    
+    if(a > b){
+        c = a + b;
+    }
+    if(b > a){
+        c = a - b;
+    }
+    if(a == b){
+        c = a * b;
     }
 }

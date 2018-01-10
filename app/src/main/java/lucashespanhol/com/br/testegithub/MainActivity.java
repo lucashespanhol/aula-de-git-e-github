9ef40ef28f8fa0210e9ee45aab20d78965ae6179
Lucas Hespanhol <lukas_bora@yahoo.com.br>
2018-01-09 22:33:59 -0200

    Alteracoes do J.Lucas

--- app/src/main/java/lucashespanhol/com/br/testegithub/MainActivity.java
+++ app/src/main/java/lucashespanhol/com/br/testegithub/MainActivity.java
@@ -5,9 +5,22 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private int a;
+    private int b;
+    private int c;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if(a != b){
+            c = a;
+        }
+        if(a == b){
+            c = a * b;
+        }
     }
+
+
 }
